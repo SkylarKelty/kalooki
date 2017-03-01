bcc4d9e9f4717b8553ade166e0b75292f75bb9a3
Skylar Kelty <skylarkelty@gmail.com>
2017-03-01 20:27:12 +0000

    Make images draggable

--- app.module.js
+++ app.module.js
@@ -1,7 +1,25 @@
 // Define the `kalooki` module
-var kalooki = angular.module('kalookiApp', []);
+var kalooki = angular.module('kalookiApp', ['ui.sortable']);
 
 // Define the `KalookiController` controller on the `kalooki` module
 kalooki.controller('KalookiController', function KalookiController($scope) {
     $scope.playername = '';
 });
+
+kalooki.buildExampleCardArray = function(size) {
+    var numbers = ['a',2,3,4,5,6,7,8,9,10,'j','q','k'];
+    var types = ['c','s','h','d'];
+    var i, array = [];
+    for (i = 1; i <= size; i++) {
+        var number = numbers[Math.floor(Math.random() * numbers.length)];
+        var cardtype = types[Math.floor(Math.random() * types.length)];
+
+        array.push({
+          src: 'media/images/cards/' + number + cardtype + '.png',
+          name: name,
+          type: cardtype
+        });
+    }
+
+    return array;
+};
--- card-table/card-table.module.js
+++ card-table/card-table.module.js
@@ -1,8 +1,24 @@
 // Register `CardTable` component, along with its associated controller and template
 angular.module('kalookiApp').component('cardTable', {
     templateUrl: 'card-table/card-table.template.html',
-    controller: function CardTableController() {
+    controller: function CardTableController($scope) {
         this.pagetitle = 'Kalooki';
         this.introtext = 'Welcome to Kalooki Online';
+        this.activecard = 'media/images/cards/2c.png'
+        this.hand = kalooki.buildExampleCardArray(14);
+
+        this.sortingLog = [];
+
+        this.sortableOptions = {
+            'ui-floating': true,
+            axis: 'x',
+            stop: function(e, ui) {
+                var logEntry = {
+                    ID: this.sortingLog.length + 1,
+                    Text: 'Moved element: ' + ui.item.scope().item.src
+                };
+                this.sortingLog.push(logEntry);
+            }
+        };
     }
 });
